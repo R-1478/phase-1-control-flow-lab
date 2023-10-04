de5070696508b81dc2b6e1353495eb97e249602d
evansthitai <evansnt9619@gmail.com>
2023-10-04 16:21:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+//  switch (feet) {
+//   case feet > 2500 :
+//  console.log ("No can do");
+//   break ;
+//   case (feet > 2000):
+//    console.log("I will gladly take your thiry bucks")
+//   break ;
+//   case (feet <= 400) :
+//    console.log("This one is on me!")
+//    break ;
+//    default:
+//     console.log("How many feet would you like to fly today?")
+//   return feet ;
+//  }
+
+  if (feet <= 400) {
+    return 'This one is on me!' }
+  else if (feet >= 2000 && feet <=2499){
+    return 'I will gladly take your thirty bucks.'}
+  else if (feet >=2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(450);
+
+function ternaryCheckCity(city){
+ if (city === 'NYC'){
+  return "Ok, sounds good."
+ } else if (city !== 'NYC')
+ return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity();
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+  return "Thank you so much." }
+  else if (tip == 'not as generous'){
+  return "Thank you." }
+  else return "Bye."
+
 }
+
+ternaryCheckCity() ;
